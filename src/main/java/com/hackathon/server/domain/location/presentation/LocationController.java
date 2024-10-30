6126bb4d2e442baff25009c6110bbfbe278b07c7
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-30 10:03:42 +0900

    add :: PostLocation api

--- src/main/java/com/hackathon/server/domain/location/presentation/LocationController.java
+++ src/main/java/com/hackathon/server/domain/location/presentation/LocationController.java
@@ -1,13 +1,13 @@
 package com.hackathon.server.domain.location.presentation;
 
+import com.hackathon.server.domain.location.presentation.dto.request.PostLocationRequestDto;
 import com.hackathon.server.domain.location.presentation.dto.response.GetLocationResponseDto;
 import com.hackathon.server.domain.location.service.GetLocationService;
+import com.hackathon.server.domain.location.service.PostLocationService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -15,9 +15,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class LocationController {
 
     private final GetLocationService getLocationService;
+    private final PostLocationService postLocationService;
 
-    @GetMapping("/{location_id}")
-    public ResponseEntity<GetLocationResponseDto> getLocation(@PathVariable("location_id") Long locationId) {
-        return ResponseEntity.ok(getLocationService.execute(locationId));
+    @GetMapping
+    public ResponseEntity<GetLocationResponseDto> getLocation() {
+        return ResponseEntity.ok(getLocationService.execute());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> postLocation(@RequestBody PostLocationRequestDto postLocationRequestDto) {
+        postLocationService.execute(postLocationRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 }
