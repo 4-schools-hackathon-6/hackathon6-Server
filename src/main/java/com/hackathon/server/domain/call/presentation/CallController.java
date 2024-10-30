b976fcb8b1c6a237026f01f5b4dd79842b2b3aaf
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-30 23:32:42 +0900

    add :: ScanScall api

--- src/main/java/com/hackathon/server/domain/call/presentation/CallController.java
+++ src/main/java/com/hackathon/server/domain/call/presentation/CallController.java
@@ -3,14 +3,18 @@ package com.hackathon.server.domain.call.presentation;
 import com.hackathon.server.domain.call.presentation.dto.request.AcceptRequest;
 import com.hackathon.server.domain.call.presentation.dto.request.CallRequest;
 import com.hackathon.server.domain.call.presentation.dto.response.PushScallReponse;
+import com.hackathon.server.domain.call.presentation.dto.response.ScanScallResponse;
 import com.hackathon.server.domain.call.service.AcceptService;
 import com.hackathon.server.domain.call.service.CallService;
 import com.hackathon.server.domain.call.service.PushService;
+import com.hackathon.server.domain.call.service.ScanScallService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
@@ -22,6 +26,7 @@ public class CallController {
     private final CallService callService;
     private final PushService pushService;
     private final AcceptService acceptService;
+    private final ScanScallService scanScallService;
 
     @PostMapping
     public ResponseEntity<Void> makeScall(@RequestBody CallRequest callRequest) {
@@ -39,5 +44,10 @@ public class CallController {
     public ResponseEntity<Void> acceptScall(@RequestBody AcceptRequest acceptRequest) {
         acceptService.execute(acceptRequest);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("/{call_id}")
+    public ResponseEntity<List<ScanScallResponse>> scanScall(@PathVariable("call_id") Long callId) {
+        return ResponseEntity.ok(scanScallService.execute(callId));
     }
 }
---
+++ src/main/java/com/hackathon/server/domain/call/presentation/dto/response/ScanScallResponse.java
@@ -0,0 +1,10 @@
+package com.hackathon.server.domain.call.presentation.dto.response;
+
+import lombok.Builder;
+import lombok.Getter;
+
+@Builder
+@Getter
+public class ScanScallResponse {
+    private Long userId;
+}
