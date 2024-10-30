0f909df078076b6d95a75bc77b587af14fddba72
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-30 15:36:05 +0900

    add :: CallNotFoundException

--- src/main/java/com/hackathon/server/global/exception/error/ErrorCode.java
+++ src/main/java/com/hackathon/server/global/exception/error/ErrorCode.java
@@ -9,7 +9,8 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode implements ErrorProperty {
     EMAIL_BAD_REQUEST(HttpStatus.BAD_REQUEST, "사용중인 이메일 입니다."),
     USER_NOT_FOUND(HttpStatus.NOT_FOUND, "유저를 찾지 못했습니다"),
-    LOCATION_NOT_FOUND(HttpStatus.NOT_FOUND, "위치를 찾지 못했습니다.");
+    LOCATION_NOT_FOUND(HttpStatus.NOT_FOUND, "위치를 찾지 못했습니다."),
+    CALL_NOT_FOUND(HttpStatus.NOT_FOUND, "콜 서비스를 찾지 못 했습니다.");
 
     private final HttpStatus status;
     private final String message;
