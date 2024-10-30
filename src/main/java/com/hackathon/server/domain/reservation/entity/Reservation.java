6d8e15ddebad33db9bdd45446748293e461683a1
Umjiseung <127853946+Umjiseung@users.noreply.github.com>
2024-10-30 11:08:06 +0900

    add :: Reservation

---
+++ src/main/java/com/hackathon/server/domain/reservation/entity/Reservation.java
@@ -0,0 +1,38 @@
+package com.hackathon.server.domain.reservation.entity;
+
+import ch.qos.logback.core.status.Status;
+import com.hackathon.server.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Builder
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String day; // 예약 날짜
+
+    private String time; // 예약 시간
+
+    private Boolean repetition; // 매주 반복 여부
+
+    private String x;
+
+    private String y;
+
+    @Enumerated(EnumType.STRING)
+    private Kind kind; // 기기 종류
+
+    @ManyToOne
+    @JoinColumn
+    private User user;
+}
